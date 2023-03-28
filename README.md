d3766c1dc6a7eaa0727ab79ac1ca16e62960c56c
G Locke <jkeys726@gmail.com>
2023-03-28 15:00:19 -0500

    Starting to update to 4 year college degree

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ https://gvlocke.github.io/Senior-Countdown/
 
 Simple website that (used to) count down to my senior graduation (It's in the past now). As I only know basic web design code, it's mostly a conglomeration of other people's stuff, specifically various Pens from [Matt Smith](https://codepen.io/AllThingsSmitty/pens/public) and [Traf](https://codepen.io/traf).<br>
 Progress bar animation uses the [Anime.js](https://animejs.com/) engine.
+
+## Roadmap
+- [ ] Make the units singular when appropriate
+- [ ] Make the 0 units disappear when appropriate
--- main.js
+++ main.js
@@ -3,8 +3,10 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
+        month = day * 30;
+        year = day * 365;
 
-        let graduation = new Date('May 21, 2022 14:45:00')
+        let graduation = new Date('May 6, 2026 14:00:00')
 
   // From Matt Smith's CodePen: https://codepen.io/AllThingsSmitty/pen/JJavZN
   
@@ -14,7 +16,9 @@
         const now = new Date().getTime(),
               distance = countDown - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("years").innerText = Math.floor(distance / (year)),
+          document.getElementById("months").innerText = Math.floor((distance % (year)) / (month)),
+          document.getElementById("days").innerText = Math.floor((distance % (month)) / (day)),
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
@@ -30,9 +34,9 @@
     // End
 
     
-    let semesterStart = 1641306600000
+    let semesterStart = 1661144400000
 let ms = Number(new Date()) - semesterStart 
-let semesterLength = 11855700000
+let semesterLength = 116949600000
 
 function gPercent() {
     return Math.round(ms/semesterLength * 100)
